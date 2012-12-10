710b7155b6eaa71d7aecc0530d06e6d8766f2834
sonicxml <sonicxml@gmail.com>
2012-12-09 19:08:25 -0500

    Python: Clean up imports
    Change-Id: I310ba0ea2577733154d4433d828204abd5a821e0
    Signed-off-by: sonicxml <sonicxml@gmail.com>

--- graphene.py
+++ graphene.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import numpy as np
 import sys
-import pylab as mpl
+from pylab import *
 
 def plotgraphene(gc):
     # MATLAB: plot(graphenecoordinates(:,1),graphenecoordinates(:,2),'o')'
-    mpl.scatter(gc[:,0],gc[:,1])
-    mpl.grid(True)
-    mpl.xlabel('Length (angstroms)')
-    mpl.ylabel('Width (angstroms)')
-    mpl.title('Graphene Lattice')
-    mpl.show()
+    scatter(gc[:,0],gc[:,1])
+    grid(True)
+    xlabel('Length (angstroms)')
+    ylabel('Width (angstroms)')
+    title('Graphene Lattice')
+    show()
     
 def Hamiltonian(gc):
     Norb = gc.shape[0]
     numH = Norb
-    x = np.tile(gc[:,0], (1, Norb))
-    y = np.tile(gc[:,1], (1, Norb))
-    z = np.tile(gc[:,2], (1, Norb))
-    R = np.sqrt(np.square(x.T-x) + np.square(y.T-y) + np.square (z.T-z))
+    x = tile(gc[:,0], (1, Norb))
+    y = tile(gc[:,1], (1, Norb))
+    z = tile(gc[:,2], (1, Norb))
+    R = sqrt(square(x.T-x) + square(y.T-y) + square (z.T-z))
     
-    H = np.zeros(shape=(numH, numH))
+    H = zeros(shape=(numH, numH))
     print numH
     for k in xrange(0, (numH)):
         for l in xrange(0, (numH)):
@@ -31,12 +30,12 @@ def Hamiltonian(gc):
 #            else:
 #                H[k,l]=0
     print H
-    np.savetxt('pythonHam.txt', H, delimiter='\t', fmt='%f')
+    savetxt('pythonHam.txt', H, delimiter='\t', fmt='%f')
     return H
     
 def main():
     # Load coordinate file as array
-    gc = np.loadtxt("graphenecoordinates.txt")
+    gc = loadtxt("graphenecoordinates.txt")
     gc.view('i8,i8,i8').sort(order=['f0','f1'], axis=0) # For 64-bit systems - for 32-bit, change 'i8' to 'i4'
     
     # Run functions
